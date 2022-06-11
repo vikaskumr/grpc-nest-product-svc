949097778b4f2b92eb34f74dcb158a4261cb7305
Vikas Kumar <vikas.kumar@energyweb.org>
2022-06-11 18:40:33 +0530

    chore: product service

---
+++ src/product/product.controller.ts
@@ -0,0 +1,64 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entity/product.entity';
+import {
+  CreateProductRequestDto,
+  DecreaseStockRequestDto,
+  FindOneRequestDto,
+} from './product.dto';
+import {
+  CreateProductResponse,
+  DecreaseStockResponse,
+  FindOneResponse,
+} from './product.pb';
+
+@Injectable()
+export class ProductController {
+  @InjectRepository(Product)
+  private readonly repository: Repository<Product>;
+
+  public async findOne({ id }: FindOneRequestDto): Promise<FindOneResponse> {
+    const product: Product = await this.repository.findOne({ where: { id } });
+
+    if (!product) {
+      return {
+        data: null,
+        error: ['Product not found'],
+        status: HttpStatus.NOT_FOUND,
+      };
+    }
+    return { data: product, error: null, status: HttpStatus.OK };
+  }
+
+  public async createProduct(
+    payload: CreateProductRequestDto,
+  ): Promise<CreateProductResponse> {
+    const product: Product = new Product();
+
+    product.name = payload.name;
+    product.sku = payload.sku;
+    product.stock = payload.stock;
+    product.price = payload.price;
+
+    await this.repository.save(product);
+
+    return { id: product.id, error: null, status: HttpStatus.OK };
+  }
+
+  public async decreaseStock({
+    id,
+  }: DecreaseStockRequestDto): Promise<DecreaseStockResponse> {
+    const product: Product = await this.repository.findOne({ where: { id } });
+
+    if (!product) {
+      return { error: ['Product not found'], status: HttpStatus.NOT_FOUND };
+    } else if (product.stock <= 0) {
+      return { error: ['Stock too low'], status: HttpStatus.CONFLICT };
+    }
+
+    this.repository.update(product.id, { stock: product.stock - 1 });
+
+    return { error: null, status: HttpStatus.OK };
+  }
+}
---
+++ src/product/product.pb.ts
@@ -0,0 +1,119 @@
+/* eslint-disable */
+import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
+import * as Long from 'long';
+import * as _m0 from 'protobufjs/minimal';
+import { Observable } from 'rxjs';
+
+export const protobufPackage = 'product';
+
+export interface CreateProductRequest {
+  name: string;
+  sku: string;
+  stock: number;
+  price: number;
+}
+
+export interface CreateProductResponse {
+  status: number;
+  error: string[];
+  id: number;
+}
+
+export interface FindOneData {
+  id: number;
+  name: string;
+  sku: string;
+  stock: number;
+  price: number;
+}
+
+export interface FindOneRequest {
+  id: number;
+}
+
+export interface FindOneResponse {
+  status: number;
+  error: string[];
+  data: FindOneData | undefined;
+}
+
+export interface DecreaseStockRequest {
+  id: number;
+}
+
+export interface DecreaseStockResponse {
+  status: number;
+  error: string[];
+}
+
+export const PRODUCT_PACKAGE_NAME = 'product';
+
+export interface ProductServiceClient {
+  createProduct(
+    request: CreateProductRequest,
+  ): Observable<CreateProductResponse>;
+
+  findOne(request: FindOneRequest): Observable<FindOneResponse>;
+
+  decreaseStock(
+    request: DecreaseStockRequest,
+  ): Observable<DecreaseStockResponse>;
+}
+
+export interface ProductServiceController {
+  createProduct(
+    request: CreateProductRequest,
+  ):
+    | Promise<CreateProductResponse>
+    | Observable<CreateProductResponse>
+    | CreateProductResponse;
+
+  findOne(
+    request: FindOneRequest,
+  ): Promise<FindOneResponse> | Observable<FindOneResponse> | FindOneResponse;
+
+  decreaseStock(
+    request: DecreaseStockRequest,
+  ):
+    | Promise<DecreaseStockResponse>
+    | Observable<DecreaseStockResponse>
+    | DecreaseStockResponse;
+}
+
+export function ProductServiceControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ['createProduct', 'findOne', 'decreaseStock'];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(
+        constructor.prototype,
+        method,
+      );
+      GrpcMethod('ProductService', method)(
+        constructor.prototype[method],
+        method,
+        descriptor,
+      );
+    }
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(
+        constructor.prototype,
+        method,
+      );
+      GrpcStreamMethod('ProductService', method)(
+        constructor.prototype[method],
+        method,
+        descriptor,
+      );
+    }
+  };
+}
+
+export const PRODUCT_SERVICE_NAME = 'ProductService';
+
+// If you get a compile-error about 'Constructor<Long> and ... have no overlap',
+// add '--ts_proto_opt=esModuleInterop=true' as a flag when calling 'protoc'.
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any;
+  _m0.configure();
+}
